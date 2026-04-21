343bbc5f77462efe0e744cd44d43c0b959063a24
DHANCHARLA DHARSHAN POWER <153969753+URK23CS1221@users.noreply.github.com>
2026-04-21 22:55:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,5 @@
-from flask import Flask, request, jsonify
+def add(a, b):
+    return a + b
 
-app = Flask(__name__)
-students = {"name": "John Doe", "roll": 1, "id": 1
-}
-next_id = 1
-
-@app.route('/students', methods=['GET'])
-def get_students():
-    return jsonify(students)
-
-@app.route('/students', methods=['POST'])
-def add_student():
-    global next_id
-    data = request.get_json()
-    student = {'id': next_id, 'name': data['name'], 'roll': data['roll']}
-    students[next_id] = student
-    next_id += 1
-    return jsonify(student), 201
-
-@app.route('/students/<int:sid>', methods=['DELETE'])
-def delete_student(sid):
-    students.pop(sid, None)
-    return jsonify({'msg': 'Deleted'})
-
-
-app.run(host='0.0.0.0', port=5000)
+def greet(name):
+    return f"Hello, {name}!"
